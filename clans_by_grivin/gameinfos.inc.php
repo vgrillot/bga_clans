4d71d86fa446b6073daf680bfd5b8bdf3c5c4836
vgrillot <grivin@gmail.com>
2020-05-09 18:48:48 +0200

    updating game informations

--- clans_by_grivin/gameinfos.inc.php
+++ clans_by_grivin/gameinfos.inc.php
@@ -10,55 +10,55 @@
 
 */
 
-$gameinfos = array( 
+$gameinfos = array(
 
 // Name of the game in English (will serve as the basis for translation) 
-'game_name' => "My Great Game",
+    'game_name' => "Clans",
 
 // Game designer (or game designers, separated by commas)
-'designer' => 'John Doe',       
+    'designer' => 'Leo Colovini',
 
 // Game artist (or game artists, separated by commas)
-'artist' => 'Jane Doe',         
+    'artist' => 'N/A',
 
 // Year of FIRST publication of this game. Can be negative.
-'year' => 2000,                 
+    'year' => 2002,
 
 // Game publisher
-'publisher' => 'My Publishing Company',                     
+    'publisher' => 'Venice Connection',
 
 // Url of game publisher website
-'publisher_website' => 'http://www.mypublishingcompany.com/',   
+    'publisher_website' => '',
 
 // Board Game Geek ID of the publisher
-'publisher_bgg_id' => 1234,
+    'publisher_bgg_id' => 131,
 
 // Board game geek ID of the game
-'bgg_id' => 4636,
+    'bgg_id' => 4636,
 
 
 // Players configuration that can be played (ex: 2 to 4 players)
-'players' => array( 2,3,4 ),    
+    'players' => array(2, 3, 4),
 
 // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
-'suggest_player_number' => null,
+    'suggest_player_number' => array(2),
 
 // Discourage players to play with these numbers of players. Must be null if there is no such advice.
-'not_recommend_player_number' => null,
+    'not_recommend_player_number' => null,
 // 'not_recommend_player_number' => array( 2, 3 ),      // <= example: this is not recommended to play this game with 2 or 3 players
 
 
 // Estimated game duration, in minutes (used only for the launch, afterward the real duration is computed)
-'estimated_duration' => 30,           
+    'estimated_duration' => 30,
 
 // Time in second add to a player when "giveExtraTime" is called (speed profile = fast)
-'fast_additional_time' => 30,           
+    'fast_additional_time' => 30,
 
 // Time in second add to a player when "giveExtraTime" is called (speed profile = medium)
-'medium_additional_time' => 40,           
+    'medium_additional_time' => 40,
 
 // Time in second add to a player when "giveExtraTime" is called (speed profile = slow)
-'slow_additional_time' => 50,           
+    'slow_additional_time' => 50,
 
 // If you are using a tie breaker in your game (using "player_score_aux"), you must describe here
 // the formula used to compute "player_score_aux". This description will be used as a tooltip to explain
@@ -66,67 +66,67 @@ $gameinfos = array(
 // Note: if you are NOT using any tie breaker, leave the empty string.
 //
 // Example: 'tie_breaker_description' => totranslate( "Number of remaining cards in hand" ),
-'tie_breaker_description' => "",
+    'tie_breaker_description' => "",
 
 // If in the game, all losers are equal (no score to rank them or explicit in the rules that losers are not ranked between them), set this to true 
 // The game end result will display "Winner" for the 1st player and "Loser" for all other players
-'losers_not_ranked' => false,
+    'losers_not_ranked' => false,
 
 // Game is "beta". A game MUST set is_beta=1 when published on BGA for the first time, and must remains like this until all bugs are fixed.
-'is_beta' => 1,                     
+    'is_beta' => 1,
 
 // Is this game cooperative (all players wins together or loose together)
-'is_coop' => 0, 
+    'is_coop' => 0,
 
 
 // Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
-'complexity' => 3,    
+    'complexity' => 2,
 
 // Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
-'luck' => 3,    
+    'luck' => 0,
 
 // Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 3,    
+    'strategy' => 4,
 
 // Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
-'diplomacy' => 3,    
+    'diplomacy' => 0,
 
 // Colors attributed to players
-'player_colors' => array( "ff0000", "008000", "0000ff", "ffa500", "773300" ),
+    'player_colors' => array("ff0000", "008000", "0000ff", "ffa500", "773300"),
 
 // Favorite colors support : if set to "true", support attribution of favorite colors based on player's preferences (see reattributeColorsBasedOnPreferences PHP method)
-'favorite_colors_support' => true,
+    'favorite_colors_support' => false,
 
 // When doing a rematch, the player order is swapped using a "rotation" so the starting player is not the same
 // If you want to disable this, set this to false
-'disable_player_order_swap_on_rematch' => false,
+    'disable_player_order_swap_on_rematch' => true,
 
 // Game interface width range (pixels)
 // Note: game interface = space on the left side, without the column on the right
-'game_interface_width' => array(
+    'game_interface_width' => array(
 
-    // Minimum width
-    //  default: 740
-    //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
-    //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
-    'min' => 740,
+        // Minimum width
+        //  default: 740
+        //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
+        //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
+        'min' => 740,
 
-    // Maximum width
-    //  default: null (ie: no limit, the game interface is as big as the player's screen allows it).
-    //  maximum possible value: unlimited
-    //  minimum possible value: 740
-    'max' => null
-),
+        // Maximum width
+        //  default: null (ie: no limit, the game interface is as big as the player's screen allows it).
+        //  maximum possible value: unlimited
+        //  minimum possible value: 740
+        'max' => null
+    ),
 
 // Game presentation
 // Short game presentation text that will appear on the game description page, structured as an array of paragraphs.
 // Each paragraph must be wrapped with totranslate() for translation and should not contain html (plain text without formatting).
 // A good length for this text is between 100 and 150 words (about 6 to 9 lines on a standard display)
-'presentation' => array(
+    'presentation' => array(
 //    totranslate("This wonderful game is about geometric shapes!"),
 //    totranslate("It was awarded best triangle game of the year in 2005 and nominated for the Spiel des Jahres."),
 //    ...
-),
+    ),
 
 // Games categories
 //  You can attribute a maximum of FIVE "tags" for your game.
@@ -135,7 +135,14 @@ $gameinfos = array(
 //  http://en.doc.boardgamearena.com/Game_meta-information:_gameinfos.inc.php
 //  IMPORTANT: this list should be ORDERED, with the most important tag first.
 //  IMPORTANT: it is mandatory that the FIRST tag is 1, 2, 3 and 4 (= game category)
-'tags' => array( 2 ),
+    'tags' => array(
+        1,  // abstract game
+        11, // medium length
+        20, // awarded (2003 Spiel des Jahres Nominee)
+        203, // worker placement (?)
+        205, // Bluffing
+        208 // Majority
+    ),
 
 
 //////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
@@ -143,8 +150,8 @@ $gameinfos = array(
 // simple : A plays, B plays, C plays, A plays, B plays, ...
 // circuit : A plays and choose the next player C, C plays and choose the next player D, ...
 // complex : A+B+C plays and says that the next player is A+B
-'is_sandbox' => false,
-'turnControl' => 'simple'
+    'is_sandbox' => false,
+    'turnControl' => 'simple'
 
 ////////
 );
