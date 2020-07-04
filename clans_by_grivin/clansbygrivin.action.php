04b9e9b9252cc6c827d7cd857ce86516e31d032f
vgrillot <grivin@gmail.com>
2020-07-04 12:55:29 +0200

    removed todo

--- clans_by_grivin/clansbygrivin.action.php
+++ clans_by_grivin/clansbygrivin.action.php
@@ -7,7 +7,7 @@
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
  * -----
- * 
+ *
  * clansbygrivin.action.php
  *
  * ClansByGrivin main action entry point
@@ -15,31 +15,26 @@
  *
  * In this file, you are describing all the methods that can be called from your
  * user interface logic (javascript).
- *       
+ *
  * If you define a method "myAction" here, then you can call it from your javascript code with:
  * this.ajaxcall( "/clansbygrivin/clansbygrivin/myAction.html", ...)
  *
  */
-  
-  
-  class action_clansbygrivin extends APP_GameAction
-  { 
+
+
+class action_clansbygrivin extends APP_GameAction
+{
     // Constructor: please do not modify
-   	public function __default()
-  	{
-  	    if( self::isArg( 'notifwindow') )
-  	    {
+    public function __default()
+    {
+        if (self::isArg('notifwindow')) {
             $this->view = "common_notifwindow";
-  	        $this->viewArgs['table'] = self::getArg( "table", AT_posint, true );
-  	    }
-  	    else
-  	    {
+            $this->viewArgs['table'] = self::getArg("table", AT_posint, true);
+        } else {
             $this->view = "clansbygrivin_clansbygrivin";
-            self::trace( "Complete reinitialization of board game" );
-      }
-  	} 
-  	
-  	// TODO: defines your action entry points there
+            self::trace("Complete reinitialization of board game");
+        }
+    }
 
 
     /*
@@ -63,21 +58,21 @@
     
     */
 
-      public function playHuts()
-      {
-          self::setAjaxMode();
+    public function playHuts()
+    {
+        self::setAjaxMode();
 
-          // Retrieve arguments
-          // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
-          $src_territory_id = self::getArg( "src_territory_id", AT_posint, true );
-          $dst_territory_id = self::getArg( "dst_territory_id", AT_posint, true );
+        // Retrieve arguments
+        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
+        $src_territory_id = self::getArg("src_territory_id", AT_posint, true);
+        $dst_territory_id = self::getArg("dst_territory_id", AT_posint, true);
 
-          // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
-          $this->game->moveHuts( $src_territory_id, $dst_territory_id );
+        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
+        $this->game->moveHuts($src_territory_id, $dst_territory_id);
 
-          self::ajaxResponse( );
-      }
+        self::ajaxResponse();
+    }
 
-  }
+}
   
 
